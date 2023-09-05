dd02a6a3b484ad15ccb2e5dd82408c8b4c43046e
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-09-05 08:10:29 -0600

    fix(GetYesNoAnswer): added YesNoModel fromJson method for http response

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:yes_no/infrastructure/models/yes_no_model.dart';
 
 import '../../domain/entities/message.dart';
 
@@ -6,6 +7,11 @@ class GetYesNoAnswer {
   final _dio = Dio(BaseOptions(baseUrl: 'https://yesno.wtf'));
   Future<Message> getAnswer() async {
     final response = await _dio.get('/api');
-    throw UnimplementedError();
+    final yesNoModel = YesNoModel.fromJson(response.data);
+    return Message(
+      text: yesNoModel.answer,
+      imageUrl: yesNoModel.image,
+      fromWho: FromWho.receiver,
+    );
   }
 }
