e0168795faf96ed084e26b2b69f06141fe6d9bb0
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-09-04 23:26:33 -0600

    feat: added multiprovider to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no/presentation/providers/char_provider.dart';
 
 import 'config/theme/app_theme.dart';
 import 'presentation/screens/chat/chat_screen.dart';
@@ -8,11 +10,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Yes No app',
-      theme: AppTheme(selectedColorTheme: 6).theme(),
-      home: const ChatScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Yes No app',
+        theme: AppTheme(selectedColorTheme: 6).theme(),
+        home: const ChatScreen(),
+      ),
     );
   }
 }
