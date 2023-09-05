540fde8fe0972116b00bd5c2bf9907cec099f39e
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-09-04 23:00:06 -0600

    feat: created message entity

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,14 @@
+enum FromWho { sender, receiver };
+
+class Message {
+  final String text;
+  final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({
+    required this.text,
+    required this.fromWho,
+    this.imageUrl
+  });
+
+}
