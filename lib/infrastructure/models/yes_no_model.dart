d4522a4f6e395b1e5f8b323d4b3f896222289a73
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-09-05 08:19:06 -0600

    feat(YesNoModel): added new method

--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,3 +1,5 @@
+import 'package:yes_no/domain/entities/message.dart';
+
 class YesNoModel {
   final String answer;
   final bool forced;
@@ -9,6 +11,18 @@ class YesNoModel {
     required this.image,
   });
 
-  factory YesNoModel.fromJson(Map<String, dynamic> json) => YesNoModel(
-      answer: json['answer'], forced: json['forced'], image: json['image']);
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() =>
+      Message(text: answer, fromWho: FromWho.receiver, imageUrl: image);
 }
