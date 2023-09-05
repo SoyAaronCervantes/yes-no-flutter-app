b58c46380795123b3bb9eea27659e4227c9fcf85
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-09-05 08:19:41 -0600

    feat(GetYesNoAnswer): change return with toMessageEntity

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -7,11 +7,7 @@ class GetYesNoAnswer {
   final _dio = Dio(BaseOptions(baseUrl: 'https://yesno.wtf'));
   Future<Message> getAnswer() async {
     final response = await _dio.get('/api');
-    final yesNoModel = YesNoModel.fromJson(response.data);
-    return Message(
-      text: yesNoModel.answer,
-      imageUrl: yesNoModel.image,
-      fromWho: FromWho.receiver,
-    );
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+    return yesNoModel.toMessageEntity();
   }
 }
