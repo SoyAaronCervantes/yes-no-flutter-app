174b00a16df2edfe90d1550fc736126a9f554e51
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-09-04 23:49:18 -0600

    feat(MessageFieldBox): added onValue proprety as ValueChange

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +25,7 @@ class MessageFieldBox extends StatelessWidget {
           onPressed: () {
             final textValue = textController.value.text;
             textController.clear();
+            onValue(textValue);
           },
         ));
 
@@ -37,6 +39,7 @@ class MessageFieldBox extends StatelessWidget {
       onFieldSubmitted: (value) {
         textController.clear();
         focusNode.requestFocus();
+        onValue(value);
       },
     );
   }
